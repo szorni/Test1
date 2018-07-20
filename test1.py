1b48c2ec6e75f3a31f68f57134f1f2d1881dc092
szorni <33416792+szorni@users.noreply.github.com>
2018-07-20 19:28:08 +0400

    Initial commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+
+def test():
+    print ("lalala")
+    input1 = input("Co mi zrobisz? Jak mnie zlapiesz?")
+    t = int(input1)*2
+    print(t)
+    print(input1)
+    return 1
+
+
+t = test()
+print(t)
+
+
+test()
